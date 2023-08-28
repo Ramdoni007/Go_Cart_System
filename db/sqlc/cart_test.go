a2b5259bc4b58e49b219745d69e49dfd3ad94b85
Ramdoni007 <ramd42156@gmail.com>
2023-08-28 10:13:18 +0700

    Pass Test For Update And Deleted cart

--- db/sqlc/cart_test.go
+++ db/sqlc/cart_test.go
@@ -2,10 +2,25 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"github.com/Ramdoni007/Go_CartSystem/util"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
+
+func CreateCartForUpdate(t *testing.T) Cart {
+	arg := CreateCartParams{
+		ID:          18,
+		NameProduct: "LAPTOP ROG 454YZ",
+		Quantity:    5,
+	}
+
+	cart, err := testQueris.CreateCart(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, cart)
+
+	return cart
+}
 
 func CreateRandomCart(t *testing.T) Cart {
 	arg := CreateCartParams{
@@ -55,5 +70,29 @@ func TestGetCart(t *testing.T) {
 }
 
 func TestUpdateCart(t *testing.T) {
-	
+	cart1 := CreateCartForUpdate(t)
+
+	arg := UpdateCartParams{
+		ID:          16,
+		NameProduct: cart1.NameProduct,
+		Quantity:    cart1.Quantity,
+	}
+	cart2, err := testQueris.UpdateCart(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, cart2)
+
+	require.Equal(t, cart1.ID, cart2.ID)
+	require.Equal(t, cart1.NameProduct, cart2.NameProduct)
+	require.Equal(t, cart1.Quantity, cart2.Quantity)
+}
+
+func TestDeletedCart(t *testing.T) {
+	cart1 := CreateCartForUpdate(t)
+	err := testQueris.DeleteCart(context.Background(), cart1.ID)
+	require.NoError(t, err)
+
+	cart2, err := testQueris.GetCart(context.Background(), cart1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, cart2)
 }
